6bc22bc79132e36550c43043b153febf0fb1ae4b
Vaishnavi99-dev <sharmavaishnavi7860@gmail.com>
2026-05-09 23:18:34 +0530

    feature added to hash password.

--- src/utils/utilityFunctions.js
+++ src/utils/utilityFunctions.js
@@ -1,5 +1,16 @@
+import bcrypt from "bcrypt";
+
 export function isNotEmpty(field) {
     if(field === ""){
         return false;
     }
 }
+
+export async function hashPassword (pass){
+    // Placeholder for hashing logic
+    return await bcrypt.hash(pass, 10);
+
+}
+
+// const hashes =  await hashPassword("yash");
+// console.log(hashes);
